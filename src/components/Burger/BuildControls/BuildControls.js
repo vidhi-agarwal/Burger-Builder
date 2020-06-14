4ef56deae743699f45b04530afa16205abde3a54
vidhi agarwal <vidhi.agarwal2018@vitstudent.ac.in>
2020-06-14 23:50:22 +0530

    removing ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,6 +17,7 @@ const buildControls = (props) => (
 				key={ctrl.label}
 				label={ctrl.label}
 				added={() => props.ingredientAdded(ctrl.type)}
+				removed={() => props.ingredientRemoved(ctrl.type)}
 			/>
 		))}
 	</div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,13 +40,27 @@ class BurgerBuilder extends Component {
 		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
 	};
 
-	removeIngredientHandler = (type) => {};
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients,
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceDeduction;
+		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+	};
 
 	render() {
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls ingredientAdded={this.addIngredientHandler} />
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+				/>
 			</Aux>
 		);
 	}
