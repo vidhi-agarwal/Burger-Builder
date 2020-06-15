4cde0fe6ef166c7dedc4ae07645556d45a427278
vidhi agarwal <vidhi.agarwal2018@vitstudent.ac.in>
2020-06-15 08:29:12 +0530

    adding animation to modal

--- src/components/Burger/OrderSummmary/OrderSummary.js
+++ src/components/Burger/OrderSummmary/OrderSummary.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxiliary/Auxiliary';
-import Button from '../../UI/Button/Button';
+import Aux from '../../../hoc/Auxiliary';
+//import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
 	//This could be a functional component, doesn't have to be a class.
@@ -24,16 +24,16 @@ class OrderSummary extends Component {
 				<h3>Your Order</h3>
 				<p>A delicious burger with the following ingrediens:</p>
 				<ul>{ingredientSummary}</ul>
-				<p>
+				{/* <p>
 					<strong>Total Price: {this.props.price.toFixed(2)}</strong>
-				</p>
+				</p> */}
 				<p>Continue to Checkout?</p>
-				<Button btnType='Danger' clicked={this.props.purchaseCancelled}>
+				{/* <Button btnType='Danger' clicked={this.props.purchaseCancelled}>
 					CANCEL
 				</Button>
 				<Button btnType='Success' clicked={this.props.purchaseContinue}>
 					CONTINUE
-				</Button>
+				</Button> */}
 			</Aux>
 		);
 	}
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends Component {
 		},
 		totalPrice: 50,
 		purchasable: false,
+		purchasing: false,
 	};
 
 	updatePurchaseState(ingredients) {
@@ -72,6 +73,10 @@ class BurgerBuilder extends Component {
 		this.updatePurchaseState(updatedIngredients);
 	};
 
+	purchaseHandler = () => {
+		this.setState({ purchasing: true });
+	};
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients,
@@ -81,7 +86,7 @@ class BurgerBuilder extends Component {
 		}
 		return (
 			<Aux>
-				<Modal>
+				<Modal show={this.state.purchasing}>
 					<OrderSummary ingredients={this.state.ingredients} />
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
@@ -90,6 +95,7 @@ class BurgerBuilder extends Component {
 					ingredientRemoved={this.removeIngredientHandler}
 					disabled={disabledInfo}
 					purchasable={this.state.purchasable}
+					ordered={this.purchaseHandler}
 					price={this.state.totalPrice}
 				/>
 			</Aux>
