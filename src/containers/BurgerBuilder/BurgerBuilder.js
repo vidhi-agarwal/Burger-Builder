a9b4f11e05a2c847a65c1fd9c41a7c6ca96fc7d1
vidhi agarwal <vidhi.agarwal2018@vitstudent.ac.in>
2020-06-15 23:30:09 +0530

    post request added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummmary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 5,
@@ -82,7 +83,25 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		alert('You Continue!');
+		//alert('You Continue!');
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Vidhi Agarwal',
+				address: {
+					street: 'Teststreet',
+					zipCode: '96749',
+					country: 'India',
+				},
+				email: 'happy@happy.com',
+			},
+			deliveryMethod: 'SuperFast',
+		};
+		axios
+			.post('/orders.json', order)
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error));
 	};
 
 	render() {
